cd1056a0b021800b2155113c0fb579fa48f352f2
lai2-24 <lai2-24@itismeucci.com>
2024-10-15 09:01:37 +0200

    secondo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,36 @@
 package com.example;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.out.println("Client partito");
+        Socket s = new Socket("localhost", 3000);
+        System.out.println("Il client si è collegato");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci una stringa: ");
+        Scanner input = new Scanner(s.getInputStream());
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String stringa;
+        do {
+            stringa = scanner.nextLine();
+            out.writeBytes(stringa + "\n");
+
+            if (input.hasNextLine()) {
+                String risposta = input.nextLine();
+                System.out.println("Risposta del server: " + risposta);
+            }
+        } while (!stringa.equals("!"));
+        
+        out.close();
+        input.close();
+        scanner.close();
+        s.close();
     }
 }
